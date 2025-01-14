03f74a8802d03bb0d63158482bdb606393cbc6a2
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2025-01-14 12:52:23 +0100

    Update routes.py

--- webse/boa205_course_announcements/routes.py
+++ webse/boa205_course_announcements/routes.py
@@ -15,14 +15,14 @@ def boa205_course_announcements_new_announcement():
         announcement = AnnouncementES(title=form.title.data, content=form.content.data, author=current_user)
         db.session.add(announcement)
         db.session.commit()
-        flash('Your annoucement has been created!', 'success')
+        flash('Kunngjøringen din er opprettet!', 'success')
         return redirect(url_for('boa205_course.boa205_course_home'))
-    return render_template('boa205_course/es_announcements/boa205_course_create_announcement.html', title='New announcement', form=form)
+    return render_template('boa205_course/boa205_announcements/boa205_course_create_announcement.html', title='New announcement', form=form)
 
 @boa205_course_announcements.route("/boa205_course/announcement/<int:announcement_id>")
 def announcement(announcement_id):
     announcement = AnnouncementES.query.get_or_404(announcement_id)
-    return render_template('boa205_course/es_announcements/boa205_course_announcement.html', title=announcement.title, announcement=announcement, func=read_image)
+    return render_template('boa205_course/boa205_announcements/boa205_course_announcement.html', title=announcement.title, announcement=announcement, func=read_image)
 
 @boa205_course_announcements.route("/boa205_course/announcement/<int:announcement_id>/update", methods=['GET', 'POST'])
 @login_required
@@ -35,12 +35,12 @@ def update_announcement(announcement_id):
         announcement.title = form.title.data
         announcement.content = form.content.data
         db.session.commit()
-        flash('Tu announcement ha sido actualizado!', 'success')
+        flash('Kunngjøringen din er oppdatert!', 'success')
         return redirect(url_for('boa205_course.boa205_course_home'))
     elif request.method == 'GET':
         form.title.data = announcement.title
         form.content.data = announcement.content
-    return render_template('boa205_course/es_announcements/boa205_course_create_announcement.html', title='Update announcement',
+    return render_template('boa205_course/boa205_announcements/boa205_course_create_announcement.html', title='Update announcement',
                            form=form, legend='Update announcement')
 
 @boa205_course_announcements.route("/boa205_course/announcement/<int:announcement_id>/delete", methods=['GET', 'POST'])
@@ -51,7 +51,7 @@ def delete_announcement(announcement_id):
         abort(403)
     db.session.delete(announcement)
     db.session.commit()
-    flash('Your announcement has been deleted!', 'success')
+    flash('Kunngjøringen din er slettet!', 'success')
     return redirect(url_for('boa205_course.boa205_course_home'))
 
 @boa205_course_announcements.route("/boa205_course/announcement/<string:username>")
@@ -62,4 +62,4 @@ def user_announcements(username):
     announcements = AnnouncementES.query.filter_by(author=user)\
         .order_by(AnnouncementES.date_posted.desc())\
         .paginate(page=page, per_page=2)
-    return render_template('boa205_course/es_announcements/boa205_course_user_announcements.html', announcements=announcements, user=user, func=read_image)
+    return render_template('boa205_course/boa205_announcements/boa205_course_user_announcements.html', announcements=announcements, user=user, func=read_image)
