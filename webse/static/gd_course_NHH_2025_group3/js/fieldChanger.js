467ce08e2ee094bc3b95282168e1017b6002ca19
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2025-04-24 16:22:23 +0200

    Update fieldChanger.js

--- webse/static/gd_course_NHH_2025_group3/js/fieldChanger.js
+++ webse/static/gd_course_NHH_2025_group3/js/fieldChanger.js
@@ -1,34 +1,39 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const methodSelect = document.getElementById('method');
-    methodSelect.value = '';
-    const itemsSelect = document.getElementById('fuel_type');
-    itemsSelect.value = '';
-    const passengersSelect = document.getElementById('passenger-select');
-    passengersSelect.style.display = 'none';
+document.addEventListener("DOMContentLoaded", function () {
+  const methodSelect = document.getElementById("method");
+  methodSelect.value = "";
+  const itemsSelect = document.getElementById("fuel_type");
+  itemsSelect.value = "";
+  const passengersSelect = document.getElementById("passenger-select");
+  passengersSelect.style.display = "none";
 
-    if (methodSelect) {
-        methodSelect.addEventListener('change', function () {
-            const method = this.value;
-            fetch(`/get-items?method=${method}`)
-                .then(response => response.json())
-                .then(data => {
-                    itemsSelect.innerHTML = '';
-                    data.items.forEach(function (item) {
-                        const option = document.createElement('option');
-                        option.value = item;
-                        option.text = item;
-                        itemsSelect.appendChild(option);
-                    });
-                })
-                .catch(error => {
-                    console.error('Error fetching items:', error);
-                });
-            if (this.value == 'Car' || this.value == 'Motorbike' || this.value == 'Scooter') {
-                passengersSelect.style.display = 'block';
-            }
-            else {
-                passengersSelect.style.display = 'none';
-            }
+  if (methodSelect) {
+    methodSelect.addEventListener("change", function () {
+      const method = this.value;
+      fetch(
+        `/green_digitalization_course/NHH/2025/group3/get-items?method=${method}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          itemsSelect.innerHTML = "";
+          data.items.forEach(function (item) {
+            const option = document.createElement("option");
+            option.value = item;
+            option.text = item;
+            itemsSelect.appendChild(option);
+          });
+        })
+        .catch((error) => {
+          console.error("Error fetching items:", error);
         });
-    }
+      if (
+        this.value == "Car" ||
+        this.value == "Motorbike" ||
+        this.value == "Scooter"
+      ) {
+        passengersSelect.style.display = "block";
+      } else {
+        passengersSelect.style.display = "none";
+      }
+    });
+  }
 });
