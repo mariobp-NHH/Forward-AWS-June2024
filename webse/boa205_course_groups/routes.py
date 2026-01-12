3e67a99966c32e53e59a39eab3b3aff86d331e8b
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2026-01-12 15:38:37 +0100

    Add files via upload

---
+++ webse/boa205_course_groups/routes.py
@@ -0,0 +1,17 @@
+from flask import render_template, url_for, Blueprint, flash, redirect, request
+from webse import application, db, bcrypt
+from webse.models import User, ChatGD, ModulsGD
+from flask_login import login_user, current_user, logout_user, login_required
+from webse.forward_users.utils import read_image
+import random
+
+boa205_course_groups= Blueprint('boa205_course_groups', __name__)
+
+@boa205_course_groups.route('/boa205_course/grupper', methods=['GET', 'POST'])
+@login_required
+def boa205_course_groups_home():
+    import random
+    n = 8
+    x = random.randint(1, n) 
+    return render_template('boa205_course/groups/groups.html', title='BØA205 Økonomistyring, grupper',
+        random_number=x)
