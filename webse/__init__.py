787406d75a8ec99ba33bbbe45bcb53ca30f87771
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2026-04-25 21:44:50 +0200

    Update __init__.py

--- webse/__init__.py
+++ webse/__init__.py
@@ -78,6 +78,7 @@ login_manager.blueprint_login_views = {
     'gd_course_NHH_2026_group2': 'gd_course_NHH_2026_group2.login',
     'gd_course_NHH_2026_group3': 'gd_course_NHH_2026_group3.login',
     'gd_course_NHH_2026_group4': 'gd_course_NHH_2026_group4.login',
+    'gd_course_NHH_2026_group5': 'gd_course_NHH_2026_group5.login',
     'sbm_course_chats': 'forward_users.forward_users_login',
     'aab_course_business_module': 'forward_users.forward_users_login',
     'aab_course_announcements': 'forward_users.forward_users_login',
@@ -150,6 +151,7 @@ from webse.gd_course_HVL_2025_group6.routes import gd_course_HVL_2025_group6
 from webse.gd_course_NHH_2026_group2.routes import gd_course_NHH_2026_group2
 from webse.gd_course_NHH_2026_group3.routes import gd_course_NHH_2026_group3
 from webse.gd_course_NHH_2026_group4.routes import gd_course_NHH_2026_group4
+from webse.gd_course_NHH_2026_group5.routes import gd_course_NHH_2026_group5
 from webse.sbm_course_home.routes import sbm_course
 from webse.sbm_course_announcements.routes import sbm_course_announcements
 from webse.sbm_course_chats.routes import sbm_course_chats
@@ -242,6 +244,7 @@ application.register_blueprint(gd_course_NHH_2025_group4)
 application.register_blueprint(gd_course_NHH_2026_group2)
 application.register_blueprint(gd_course_NHH_2026_group3)
 application.register_blueprint(gd_course_NHH_2026_group4)
+application.register_blueprint(gd_course_NHH_2026_group5)
 application.register_blueprint(sbm_course)
 application.register_blueprint(sbm_course_announcements)
 application.register_blueprint(sbm_course_chats)
